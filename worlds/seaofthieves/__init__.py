a16df50dc63e6cf113c34ef3a2e79d237c369652
Ethan <esteidl@live.com>
2024-07-07 13:28:32 -0500

    Updated client name

--- worlds/seaofthieves/__init__.py
+++ worlds/seaofthieves/__init__.py
@@ -35,7 +35,7 @@ def launch_client() -> None:
 
 LauncherComponents.components.append(
     LauncherComponents.Component(
-        "Sea of Thieves Mega Client",
+        "Sea of Thieves ALPHA Client",
         func=launch_client,
         component_type=LauncherComponents.Type.CLIENT
     )
